23bc5db0440ede38aa1a57914311dc62b214bf0a
rimorais <61158317+rimorais@users.noreply.github.com>
2021-03-14 18:55:19 +0000

    Layout Pag. Principal - Validacao Campos Vazios ou so com Espacos em Branco Finalizado;

--- app/src/main/java/com/example/reportarmais/MainActivity.kt
+++ app/src/main/java/com/example/reportarmais/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.reportarmais
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun validarLogin(view: View) {
+
+        val email = findViewById<EditText>(R.id.editText).text.toString().trim()
+        val pwd = findViewById<EditText>(R.id.editText2).text.toString().trim()
+
+        if (email.isNullOrEmpty() || pwd.isNullOrEmpty()) {
+
+            Toast.makeText(this, "Não vais passar", Toast.LENGTH_SHORT).show()
+
+        }
 
     }
 }

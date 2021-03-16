d39f5db472adaefa0522288f53f978026fe997b1
rimorais <61158317+rimorais@users.noreply.github.com>
2021-03-16 21:56:48 +0000

    Criar BD Local - Criada a Entidade Nota;

---
+++ app/src/main/java/com/example/reportarmais/entities/Nota.kt
@@ -0,0 +1,16 @@
+package com.example.reportarmais.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "nota_table")
+
+class Nota(
+
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    @ColumnInfo(name = "titulo") val titulo: String,
+    @ColumnInfo(name = "conteudo") val conteudo: String,
+    @ColumnInfo(name = "data") val data: String
+
+    )

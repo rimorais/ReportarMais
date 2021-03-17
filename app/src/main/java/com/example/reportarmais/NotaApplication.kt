9262b8d4537b5b589ff86fc68f76989ee51662a6
rimorais <61158317+rimorais@users.noreply.github.com>
2021-03-17 19:43:32 +0000

    Fazer Layout Pág. Notas - Popular BD;

--- app/src/main/java/com/example/reportarmais/NotaApplication.kt
+++ app/src/main/java/com/example/reportarmais/NotaApplication.kt
@@ -3,10 +3,16 @@ package com.example.reportarmais
 import android.app.Application
 import com.example.reportarmais.db.NotaDB
 import com.example.reportarmais.db.NotaRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class NotaApplication : Application() {
+
+    // No need to cancel this scope as it'll be torn down with the process
+    val applicationScope = CoroutineScope(SupervisorJob())
+
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
-    val database by lazy { NotaDB.getDatabase(this) }
+    val database by lazy { NotaDB.getDatabase(this, applicationScope) }
     val repository by lazy { NotaRepository(database.notaDao()) }
 }

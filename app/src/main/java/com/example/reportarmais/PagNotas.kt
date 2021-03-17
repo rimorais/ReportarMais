2dbd40733dd5a2733de5bc94577c4f8949ad6b7f
rimorais <61158317+rimorais@users.noreply.github.com>
2021-03-17 16:50:34 +0000

    Fazer Layout Pág. Notas - Continuar Recycler 1;

--- app/src/main/java/com/example/reportarmais/PagNotas.kt
+++ app/src/main/java/com/example/reportarmais/PagNotas.kt
@@ -2,11 +2,20 @@ package com.example.reportarmais
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.reportarmais.adapters.NotaAdapter
 
 class PagNotas : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pag_notas)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = NotaAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }

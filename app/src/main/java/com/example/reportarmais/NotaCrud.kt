0a73e4447ef8506fe9504585f6c920a91bfe1160
rimorais <61158317+rimorais@users.noreply.github.com>
2021-04-09 09:20:54 +0100

    HotFix - Room Activity Nota Crud

---
+++ app/src/main/java/com/example/reportarmais/NotaCrud.kt
@@ -0,0 +1,12 @@
+package com.example.reportarmais
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotaCrud : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nota_crud)
+    }
+}
